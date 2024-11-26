c8d12e14014a31212b601a9161c4f77431567b8f
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-11-26 13:59:39 +0200

    Add files via upload

---
+++ 13.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 3.Найти s = min(a, b) + min(c, d), 
+            //используя вспомогательные функции
+            //(ввода аргументов, нахождения минимальной величины, вывода результата).
+            int a = Input("a=");
+            int b = Input("b=");
+            int c = Input("c=");
+            int d = Input("d=");
+            int s = Min(a, b) + Min(c, d);
+            Console.Write(s);
+            Console.Read();
+
+        }
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        public static int Input(string a)
+        {
+            Console.Write(a);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+    
+}
+
